290372b8b552105259ae5e6c357ed935ae1dfef6
rchaser53 <t.yoshizawa@sorabito.com>
2017-11-14 12:17:37 +0900

    add pagination

--- src/stories/VueTable.vue
+++ src/stories/VueTable.vue
@@ -1,17 +1,50 @@
 <template>
-  <vuetable ref="vuetable"
-    api-url="https://vuetable.ratiw.net/api/users"
-    :fields="['name', 'email', 'birthdate']"
-  ></vuetable>
+  <div>
+    <vuetable ref="vuetable"
+      api-url="https://vuetable.ratiw.net/api/users"
+      :fields="fields"
+      pagination-path=""
+      @vuetable:pagination-data="onPaginationData"
+    ></vuetable>
+    <vuetable-pagination ref="pagination"
+      @vuetable-pagination:change-page="onChangePage"
+    ></vuetable-pagination>
+  </div>
 </template>
 
 <script>
-import Vuetable from 'vuetable-2'
+import {Vuetable, VuetablePagination} from 'vuetable-2'
 
 export default {
   name: 'vue-table',
+  data: function() {
+    return {
+      fields: [
+        'name', 'email'
+      ]
+    }
+  },
   components: {
-    Vuetable
+    Vuetable,
+    VuetablePagination
+  },
+  methods: {
+    onPaginationData (paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData)
+    },
+    onChangePage (page) {
+      this.$refs.vuetable.changePage(page)
+    }
   }
 }
 </script>
+
+<style>
+  .pagination a.item {
+    background-color: aqua;
+    display: inline-block;
+    text-align: center;
+    margin: 3px;
+    width: 30px;
+  }
+</style>
