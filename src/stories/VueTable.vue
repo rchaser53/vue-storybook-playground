0c72e85ef130532139d313b0e926db841d76f5aa
rchaser53 <t.yoshizawa@sorabito.com>
2017-11-14 11:37:51 +0900

    try to show vue-table2 in storybook

---
+++ src/stories/VueTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <vuetable ref="vuetable"
+    api-url="https://vuetable.ratiw.net/api/users"
+    :fields="['name', 'email', 'birthdate']"
+  ></vuetable>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2'
+
+export default {
+  name: 'vue-table',
+  components: {
+    Vuetable
+  }
+}
+</script>
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,9 +2,14 @@ import Vue from 'vue';
 
 import { storiesOf } from '@storybook/vue';
 import MyButton from './Button.vue';
+import VueTable from './VueTable.vue';
 
 storiesOf('MyButton', module)
   .add('story as a component', () => ({
     components: { MyButton },
     template: '<my-button :rounded="true">rounded</my-button>'
+  }))
+  .add('vue-table2', () => ({
+    components: { VueTable },
+    template: '<vue-table :rounded="true">rounded</vue-table>'
   }));
