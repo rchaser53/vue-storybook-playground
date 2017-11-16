e057e6a9b5e300e8ba4a971253f09f228156d398
rchaser53 <t.yoshizawa@sorabito.com>
2017-11-16 16:00:16 +0900

    try to use element sorting table

---
+++ src/stories/element/Table.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-table :data="tableData" :default-sort="{prop: 'date', order: 'descending'}" style="width: 100%">
+      <el-table-column prop="date" label="Date" sortable width="180">
+      </el-table-column>
+      <el-table-column prop="name" label="Name" width="180">
+      </el-table-column>
+      <el-table-column prop="address" label="Address" :formatter="formatter">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import { ElTable } from 'element-table'
+  import { ElTableColumn } from 'element-table'
+
+  export default {
+    data: function() {
+      return {
+        tableData: [
+          {
+            date: '2017/4/2',
+            name: 'hoge2',
+            address: 'mi2yajo'
+          },
+          {
+            date: '2017/5/2',
+            name: 'hoge',
+            address: 'mi3yajo'
+          },
+          {
+            date: '2017/3/2',
+            name: 'hoge',
+            address: 'mi1yajo'
+          }
+        ]
+      }
+    },
+    components: {
+      'el-table': ElTable,
+      'el-table-column': ElTableColumn
+    },
+    name: 'element-table'
+  }
+</script>
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue';
 
 import { storiesOf } from '@storybook/vue';
-import MyButton from './Button.vue';
+import MyButtons from './Button.vue';
 import VueTable from './VueTable/VueTable.vue';
+import ElementTable from './element/Table.vue';
 
 storiesOf('MyButton', module)
   .add('story as a component', () => ({
-    components: { MyButton },
+    components: { MyButtons },
     template: '<my-button :rounded="true">rounded</my-button>'
   }))
   .add('vue-table2', () => ({
     components: { VueTable },
     template: '<vue-table :rounded="true">rounded</vue-table>'
+  }))
+  .add('element vue table', () => ({
+    components: { ElementTable },
+    template: '<element-table ></element-table>'
   }));
